cab774ac96f56fe3a9c56a9d6a83f3fa57b6b55c
dChancellor <me@chancellor.tech>
2021-07-21 04:10:46 -0500

    Auth functional again, missing logout

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,22 +2,14 @@ const { Router } = require('express');
 const router = Router();
 
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
 const { db, googleAuthStrategy } = require('../lib/config');
 
 require('dotenv').config();
 
 passport.use(
-  new GoogleStrategy(googleAuthStrategy, async function googleAuthenicate(
-    req,
-    accessToken,
-    refreshToken,
-    profile,
-    done,
-  ) {
-    if (req.user) return done(null, req.user);
+  new GoogleStrategy(googleAuthStrategy, async function (req, accessToken, refreshToken, profile, done) {
     let user = await db.getUserById(profile.id);
-    console.log('user from db', user);
     if (user) return done(null, user);
     const failedUser = {
       id: profile.id,
@@ -29,53 +21,13 @@ passport.use(
   }),
 );
 
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//       callbackURL: process.env.GOOGLE_CALLBACK_URL,
-//       passReqToCallback: true,
-//     },
-//     function googleAuthenicate(req, accessToken, refreshToken, profile, done) {
-//       // if no existing user, check database for approved user
-//       if (!req.user) {
-//         // Find user in database
-//         db.getUserById(profile.id).then((currentUser) => {
-//           console.log('HERE');
-//           if (currentUser) {
-//             console.log('CURRENT USER', currentUser);
-//             done(null, currentUser);
-//             // Pass unauthorized if user not found in database
-//           } else {
-//             const failedUser = {
-//               id: profile.id,
-//               displayName: profile.displayName,
-//               email: profile.emails[0].value,
-//             };
-//             console.log(failedUser);
-//             const created = db.addFailedLogin(failedUser);
-//             done(null, false);
-//           }
-//         });
-//         // Existing user found
-//       } else {
-//         console.log('Token Found');
-//         return done(null, req.user);
-//       }
-//     },
-//   ),
-// );
-
 passport.serializeUser((user, done) => {
-  console.log('serialize user', user);
-  done(null, user._id);
+  done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  console.log('DE-ID', id);
-  db.getUserById(id).then((user) => {
-    done(null, user);
+  db.getUserById(id).then(({ id }) => {
+    done(null, id);
   });
 });
 
@@ -90,8 +42,9 @@ router.get(
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email', 'openid'] }));
 
 router.get('/logout', (req, res) => {
+  console.log(req.user);
   req.logout();
-  res.redirect(`${process.env.DOMAIN}`);
+  res.redirect(`/`);
 });
 
 module.exports = router;
